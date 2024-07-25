67895c824e4a390a8a1ccb5468713213d23f0449
cesaranayaplai <cesar.anaya@plai.mx>
2024-07-25 11:48:33 -0600

    Cambio en el footer Enlaces útiles

--- resources/views/client_public/inicio.blade.php
+++ resources/views/client_public/inicio.blade.php
@@ -784,7 +784,7 @@
           </div>
 
           <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
+            <h4>Enlaces Útiles</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="/inicio">Página principal</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="inicio#about">Nosotros</a></li>
